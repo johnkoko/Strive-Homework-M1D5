ade05efe24327955fdb7749e6eef19983f1f75c5
johnkoko <nkorjohn@gmail.com>
2021-09-24 14:00:10 +0100

    Exercise 1 completed

--- M1-D5.js
+++ M1-D5.js
@@ -7,69 +7,76 @@ ASSIGNMENT RULES
 - You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
 - The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
 */
+const writeHeader = function(title) {
+    console.log(`\n\n>----------==[ ${title.toUpperCase()} ]==----------<`)
+}
+
 
 /* EXERCISE 1
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
+writeHeader("EXERCISE 1")
 
-/* WRITE YOUR ANSWER HERE */
+const area = (l1, l2) => l1 * l2
+const Result = area(7, 5)
+console.log(Result)
 
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
-
+writeHeader("EXERCISE 2")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
  It should return triple their absolute difference if the given number is greater than 19.
 */
-
+writeHeader("EXERCISE 3")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
-
+writeHeader("EXERCISE 4")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
-
+writeHeader("EXERCISE 5")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
-
+writeHeader("EXERCISE 6")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
-
+writeHeader("EXERCISE 7")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
-
+writeHeader("EXERCISE 8")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
-
+writeHeader("EXERCISE 9")
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
-
+writeHeader("EXERCISE 10")
 /* WRITE YOUR ANSWER HERE */
 
 /* WHEN YOU ARE FINISHED
